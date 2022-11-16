c24dbea77be53d0ef75a0351e1cfdba8d4608c5c
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-11-17 00:48:02 +0900

    [modify] 색 테마 변경 & home custom

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <span>Deploy your own!</span>
+        <span>明年一定是个好年。一定是好年。</span>
+        {/* <span>Deploy your own!</span>
         <ExtLink href="https://vercel.com/new/git/external?repository-url=https://github.com/ijjk/notion-blog/tree/main&project-name=notion-blog&repository-name=notion-blog">
           <img
             src="https://vercel.com/button"
@@ -18,7 +19,7 @@ export default function Footer() {
           <ExtLink href="https://github.com/ijjk/notion-blog">
             view source
           </ExtLink>
-        </span>
+        </span> */}
       </footer>
     </>
   )
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,10 +8,9 @@ const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
   { label: 'Blog', page: '/blog' },
   { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
-const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
+// const ogImageUrl = 'https://notion-blog.now.sh/blog_thumbnail.png'
 
 const Header = ({ titlePre = '' }) => {
   const { pathname } = useRouter()
@@ -19,16 +18,15 @@ const Header = ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>
+          {titlePre ? `${titlePre} |` : ''} Jungmin Yoon | FrontEnd Developer
+        </title>
         <meta
           name="description"
-          content="An example Next.js site using Notion for the blog"
+          content="뚜벅뚜벅 걸어가는 프론트엔드 개발자"
         />
-        <meta name="og:title" content="My Notion Blog" />
-        <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={ogImageUrl} />
+        <meta name="og:title" content="Jungmin Yoon | FrontEnd Developer" />
+        <meta property="og:image" content={'../../public/blog_thumbnail.png'} />
       </Head>
       <ul>
         {navItems.map(({ label, page, link }) => (
